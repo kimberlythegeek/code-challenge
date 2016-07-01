7cbc87e60ed73f58b5e8e02661dcd366a9e78855
Kimberly Pennington <kimberlythegeek@gmail.com>
2016-07-01 16:36:11 -0600

    added pagination and navigation buttons. added total review to header. changle table to div of divs.

--- proxy.php
+++ proxy.php
@@ -1,7 +1,10 @@
 <?php
+$page = $_GET['page'];
+//if(empty($page)) { $page = 1; }
+$offset = (((int)$page - 1) * 10);
 
 /* proxy.php */
-$url = "http://test.localfeedbackloop.com/api?apiKey=61067f81f8cf7e4a1f673cd230216112&noOfReviews=10&internal=1&yelp=1&google=1&offset=50&threshold=1";
+$url = "http://test.localfeedbackloop.com/api?apiKey=61067f81f8cf7e4a1f673cd230216112&noOfReviews=10&internal=1&yelp=1&google=1&offset=" . (string)$offset . "&threshold=1";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
--- scripts.js
+++ scripts.js
@@ -1,9 +1,32 @@
-var myURL = 'http://test.localfeedbackloop.com/api?apiKey=61067f81f8cf7e4a1f673cd230216112&format=json&noOfReviews=10&internal=1&yelp=1&google=1&offset=50&threshold=1';
+var current_page = 1;
+var star_full = '<i class="fa fa-star" aria-hidden="true"></i>';
+var star_half = '<i class="fa fa-star-half-o" aria-hidden="true"></i>';
+var star_empty =  '<i class="fa fa-star-o" aria-hidden="true"></i>';
 
+function getID(page_link){
+  current_page = page_link.id;
+  apiCall(current_page);
+}
 
-$(document).ready(function(){
+function apiCall(page){
 
-  $.getJSON("http://www.acodeabove.com/code-challenge/proxy.php", function(json) {
+  $.getJSON('http://www.acodeabove.com/code-challenge/proxy.php', {page: page}, function(json) {
+
+    if(current_page==1){
+      $('.first').addClass('hidden');
+      $('.last').removeClass('hidden');
+    }
+    else if(current_page==17){
+      $('.first').removeClass('hidden');
+      $('.last').addClass('hidden')
+    }
+    else{
+      $('.first').removeClass('hidden');
+      $('.last').removeClass('hidden');
+    }
+
+    var total_rating = json.business_info.total_rating;
+    var avg_rating = total_rating.total_avg_rating;
     $.each(json.business_info, function(i){
       json.business_info[i] = JSON.stringify(json.business_info[i]).replace(/\"+/g,'');
     });
@@ -16,13 +39,24 @@ $(document).ready(function(){
     $('.name').html('<h1>' + business_info.business_name);
     $('.address').html(business_info.business_address);
     $('.phone').html('<a href="tel:1' + business_info.business_phone.replace(/[\s\-\(\)]/g,'') + '">' + business_info.business_phone + '</a>');
-    $('.ratings').html(business_info.total_rating.total_avg_rating);
-    $('.ratings').html(business_info.total_rating.total_no_of_reviews);
-    $('.url').append('<a href = ' + business_info.external_url + '>Add a Review</a>');
-    $('.url').append('<br><a href = ' + business_info.external_page_url + '>See more reviews</a>');
+      if(avg_rating%parseInt(avg_rating) > 0){
+        $('#star_rating').html(star_full.repeat(parseInt(avg_rating)) + star_half + star_empty.repeat(5 - parseInt(avg_rating) - 1));
+      }
+      else {
+        $('#star_rating').html(star_full.repeat(parseInt(avg_rating)) + star_empty.repeat(5 - avg_rating));
+      }
+    $('#total_rating').html('Average Rating: ' + avg_rating + ' out of ' + total_rating.total_no_of_reviews + ' reviews');
+    $('#add_review').html('<a href = ' + business_info.external_url + '>Add a Review</a>');
+    $('#more_reviews').html('<br><a href = ' + business_info.external_page_url + '>See more reviews</a>');
     $('.map').html(map);
 
+    var page_numbers = '<ul class="pagination pagination-sm">';
+    for(i=1; i<18; i++){ page_numbers += '<li><a id="' + i + '" href=# onclick="javascript:getID(this)">' + i + '</a></li>'; }
+    page_numbers += '</ul>';
+    $('.page').html(page_numbers);
+
     $.each(reviews,function(i){
+      console.log(i);
       var review = reviews[i];
       $.each(review, function(j){
         review[j] = JSON.stringify(review[j]).replace(/\"+/g,'');
@@ -41,17 +75,22 @@ $(document).ready(function(){
         default:
           review_from = 'invalid source';
       }
-      var star_full = '<i class="fa fa-star" aria-hidden="true"></i>';
-      var star_empty =  '<i class="fa fa-star-o" aria-hidden="true"></i>';
+      var num = i+1;
       var review_link = review.customer_url + '?review_id=' + review.review_id;
       review.rating = parseInt(review.rating);
-      $('tbody').append('<tr id="review_' + i + '">');
-      $('#review_' + i).append('<td class="rating">' + star_full.repeat(review.rating) + star_empty.repeat(5-review.rating) + '</td>');
-      $('#review_' + i).append('<td class="customer_name">' + '<a href="' + review_link + '">' + review.customer_name + '</a></td>');
-      $('#review_' + i).append('<td class="description">' + review.description + '</td>');
-      $('#review_' + i).append('<td class="review_from"><img src="' + review_from + '"></td></tr>');
-      $('#review_' + i).on('click',function(){location.href=review_link});
+      $('#review_' + num + ' .rating').html(star_full.repeat(review.rating) + star_empty.repeat(5-review.rating));
+      $('#review_' + num + ' .customer_name').html('<a href="' + review_link + '">' + review.customer_name + '</a>');
+      $('#review_' + num + ' .review_from').html('<img src="' + review_from + '">');
+      $('#review_' + num + ' .description').html('"' + review.description + '"');
+      $('#review_' + num).on('click',function(){location.href=review_link});
     });
+  });
+}
 
-  })
+$(document).ready(function(){
+
+  apiCall(current_page);
+
+
+
 });
